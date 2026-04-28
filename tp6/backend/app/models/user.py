cb5c876c19d4d07a61708061af306e5111fd02ba
Jaodem <orellana.joseaugusto@gmail.com>
2026-04-28 16:36:40 -0300

    feat: definir modelo de usuario con SQLModel

---
+++ tp6/backend/app/models/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy.sql.expression import table
+from sqlmodel import SQLModel, Field
+from typing import Optional
+
+# Clase base para los usuarios
+class UserBase(SQLModel):
+    name: str
+    email: str = Field(unique=True, index=True)
+
+# Modelo para la base de datos
+class User(UserBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    hashed_password: str
+
+# Esquema para la creación de usuarios
+class UserCreate(UserBase):
+    password: str
